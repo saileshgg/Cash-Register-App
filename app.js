1b6c0cabad11d9fa03109de8eeab5d77e32137a3
saileshgg <saileshgurung36@gmail.com>
2022-01-07 15:15:35 +0530

    upto tables

--- app.js
+++ app.js
@@ -1,16 +1,38 @@
-var billInput = document.querySelector("#bill");
-var cashInput = document.querySelector("#cash");
+notes = [2000, 500, 100, 20, 10, 5, 1]
 
+var input1 = document.querySelector("#bill");
+var input2 = document.querySelector("#cash");
 var button = document.querySelector("#check");
+
+var denomination = [];
 
 function clickHandler() {
 
-    if(billInput < 0 || cashInput < billInput) {
+    var billAmount = input1.value;
+    var cashAmount = input2.value;
+
+    if(billAmount < 0) {
+        console.log("Invalid Input 1");
+    } else if(billAmount > cashAmount) {
+        console.log("Invalid Input 2");
+    }
+    else {
+        var returnAmount = cashAmount - billAmount;
         
+        for(var i=0;i<notes.length;i++) {
+            var note = Math.floor(returnAmount/notes[i]);
+            denomination[i] = note;
+            returnAmount = returnAmount % notes[i];
+            if(returnAmount === 0) {
+                break;
+            }
+        }
+
+        console.log(denomination);
     }
 
 
-    console.log("Button Clicked");
+    
 }
 
 button.addEventListener("click", clickHandler);
